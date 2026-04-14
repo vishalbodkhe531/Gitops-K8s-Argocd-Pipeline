e4cf7ac8d77709b8100a73a157f7affad70dd5c7
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2026-04-14 11:37:57 +0530

    Done basic node app setup

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    status: "success",
+    message: "Welcome to the Node.js GitOps CI/CD pipeline powered by ArgoCD",
+    service: "nodejs-gitops-k8s-argocd-pipeline",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
