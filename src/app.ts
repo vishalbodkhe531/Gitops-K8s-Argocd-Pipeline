53c4f380fd3357a2953a506b907a88c8387f7bf8
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2026-04-26 09:46:24 +0530

    testing chanages 4.0

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.json({
     status: "success",
-    message: "Auto Deploy Working via ArgoCD + GitHub Actions (change-3.0)",
+    message: "Auto Deploy Working via ArgoCD + GitHub Actions (change-4.0)",
     service: "nodejs-gitops-k8s-argocd-pipeline",
   });
 });
